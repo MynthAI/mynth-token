41c44d073be7698d9b5516f16e4b4979844b7bac
SynthLuvr <131367121+SynthLuvr@users.noreply.github.com>
2023-05-19 11:33:42 +0200

    Refactored

--- register.sh
+++ register.sh
@@ -10,60 +10,69 @@
 # To update existing metadata, pass `--changed` followed by a
 # list of fields that have been changed. For example:
 #
-#    bash register.sh logo description
+#     bash register.sh logo description
 
 set -e
 
 string_in_array() {
-  local string="$1"
-  shift
-  local array=("$@")
+    local string="$1"
+    shift
+    local array=("$@")
 
-  for element in "${array[@]}"; do
-    if [[ "$element" == "$string" ]]; then
-      return 0
-    fi
-  done
+    for element in "${array[@]}"; do
+        if [[ "$element" == "$string" ]]; then
+            return 0
+        fi
+    done
 
-  return 1
+    return 1
 }
 
-name=$(yq '.name' metadata.yaml)
-description=$(yq '.description' metadata.yaml)
-ticker=$(yq '.ticker' metadata.yaml)
-url=$(yq '.url' metadata.yaml)
-logo=$(yq '.logo' metadata.yaml)
-decimals=$(yq '.decimals' metadata.yaml)
+# Get the user-specified fields that have changed, if any
+changed=()
+if [ "$1" == "--changed" ]; then
+    shift
+    changed=("$@")
+fi
+
+get_metadata_value() {
+    local field="$1"
+    local default_value="$2"
+    if string_in_array "$field" "${changed[@]}"; then
+        echo "$default_value"
+    else
+        yq ".$field" metadata.yaml
+    fi
+}
+
+name=$(get_metadata_value "name" "Null")
+description=$(get_metadata_value "description" "Null")
+ticker=$(get_metadata_value "ticker" "Null")
+url=$(get_metadata_value "url" "Null")
+logo=$(get_metadata_value "logo" "n.png")
+decimals=$(get_metadata_value "decimals" "Null")
 
 encodedName=$(echo -n "$name" | xxd -ps)
 policyID=$(cat policy/policy.id)
 token="$policyID$encodedName"
 
-# Get the user-specified fields that have changed, if any
-changed=()
-if [ "$1" == "--changed" ]; then
-  shift
-  changed=("$@")
-fi
-
-n="Null"
 token-metadata-creator entry --init "$token" \
-  --name "$(if string_in_array name "${changed[@]}"; then echo "$n"; else echo "$name"; fi)" \
-  --description "$(if string_in_array description "${changed[@]}"; then echo "$n"; else echo "$description"; fi)" \
-  --ticker "$(if string_in_array ticker "${changed[@]}"; then echo "$n"; else echo "$ticker"; fi)" \
-  --url "$(if string_in_array url "${changed[@]}"; then echo "$n"; else echo "$url"; fi)" \
-  --logo "$(if string_in_array logo "${changed[@]}"; then echo "n.png"; else echo "$logo"; fi)" \
-  --decimals "$(if string_in_array decimals "${changed[@]}"; then echo "$n"; else echo "$decimals"; fi)" \
-  --policy policy/policy.script
+    --name "$name" \
+    --description "$description" \
+    --ticker "$ticker" \
+    --url "$url" \
+    --logo "$logo" \
+    --decimals "$decimals" \
+    --policy policy/policy.script
 
 token-metadata-creator entry "$token" \
-  --name "$name" \
-  --description "$description" \
-  --ticker "$ticker" \
-  --url "$url" \
-  --logo "$logo" \
-  --decimals "$decimals" \
-  --policy policy/policy.script
+    --name "$(yq '.name' metadata.yaml)" \
+    --description "$(yq '.description' metadata.yaml)" \
+    --ticker "$(yq '.ticker' metadata.yaml)" \
+    --url "$(yq '.url' metadata.yaml)" \
+    --logo "$(yq '.logo' metadata.yaml)" \
+    --decimals "$(yq '.decimals' metadata.yaml)" \
+    --policy policy/policy.script
 
 token-metadata-creator entry "$token" -a policy/policy.skey
 token-metadata-creator entry "$token" --finalize
